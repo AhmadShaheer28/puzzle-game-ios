60c12a9ea3bec552f0c54a600481a296b2fc1641
Ahmad Shaheer <ahmadshaheer28@gmail.com>
2024-06-07 04:06:32 +0500

    file structure updated

---
+++ PuzzleGame/App/PuzzleGameApp.swift
@@ -0,0 +1,18 @@
+//
+//  PuzzleGameApp.swift
+//  PuzzleGame
+//
+//  Created by Ahmad Shaheer on 07/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct PuzzleGameApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
---
+++ PuzzleGame/Views/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  PuzzleGame
+//
+//  Created by Ahmad Shaheer on 07/06/2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
